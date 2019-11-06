477381337cde9d15a5d0894fd93cbf38b29c4993
fireplacetv <derrick.low@getaround.com>
2019-11-05 19:27:44 -0800

    move import back up

--- gifaround.js
+++ gifaround.js
@@ -5,9 +5,9 @@
  *  - Example Visualizations - https://github.com/looker/custom_visualizations_v2/tree/master/src/examples
  **/
 
-
+import "https://code.jquery.com/jquery-2.2.4.min.js";
+  
 const visObject = {
-  import    "https://code.jquery.com/jquery-2.2.4.min.js";
   // import    "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js";
   // import    "https://cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.js";
 
