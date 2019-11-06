88a167bcc7a833fecf85a71f13161f93c1afb429
fireplacetv <derrick.low@getaround.com>
2019-11-05 18:50:27 -0800

    move credential out of source

--- gifaround.js
+++ gifaround.js
@@ -8,7 +8,7 @@
 import    "https://code.jquery.com/jquery-2.2.4.min.js";
 import    "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js";
 import    "https://cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.js";
-
+const credentials = import("./credentials.json");
 
 const visObject = {
  /**
@@ -49,7 +49,7 @@ const visObject = {
     console.log(looker_data);
     
     var xhr = $.get("https://api.giphy.com/v1/gifs/random",
-                    {"api_key":"ITAuSlrn0baNIHP6x3IMIMJxZHNiuLKr"});
+                    {"api_key": credentials.api_key});
     xhr.done(function(xhr_data) {
       var viz_gif = document.createElement("IMG");
       viz_gif.setAttribute("src", xhr_data.data.images.original.url);
